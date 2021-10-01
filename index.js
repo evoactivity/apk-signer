95adfc3c35948663b5a88f89440233ef5ab69556
Liam Potter <liam@liampotter.co.uk>
2021-10-01 14:58:36 +0100

    use apksigner for v2 signing

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
-var fs = require('q-io/fs')
-var path = require('path')
-var exec = require('child-process-promise').exec
+var fs = require('q-io/fs');
+var path = require('path');
+var exec = require('child-process-promise').exec;
 
 module.exports = function (config) {
-  var unalignedPath = config.output ? config.output + '.apk' : path.resolve(config.name + '-unaligned.apk')
-  
-  var jarsignerCmd = ['jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1']
-  jarsignerCmd.push('-keystore ' + config.keystore)
-  jarsignerCmd.push('-storepass ' + config.storepass)
-  jarsignerCmd.push('-keypass ' + config.keypass)
-  jarsignerCmd.push('-signedjar ' + unalignedPath)
-  jarsignerCmd.push(config.file)
-  jarsignerCmd.push(config.alias)
+  var unalignedPath = config.output
+    ? config.output + '.apk'
+    : path.resolve(config.name + '-unaligned.apk');
 
-  var zipalignCmd = ['zipalign -f -v 4']
-  zipalignCmd.push(unalignedPath)
-  zipalignCmd.push(config.name + '.apk')
+  // apksigner sign --ks mykeystorefile --ks-key-alias myaliasname myapkfile.apk
+  var apksignerCmd = ['apksigner sign'];
+  apksignerCmd.push('--ks ' + config.keystore);
+  apksignerCmd.push('--ks-pass ' + config.storepass);
+  apksignerCmd.push('--key-pass ' + config.keypass);
+  apksignerCmd.push('--ks-key-alias ' + config.alias);
+  apksignerCmd.push('--in ' + config.file);
+  apksignerCmd.push('--out ' + unalignedPath);
 
-  var log = ''
-  return exec(jarsignerCmd.join(' '), {
+  var zipalignCmd = ['zipalign -f -v 4'];
+  zipalignCmd.push(unalignedPath);
+  zipalignCmd.push(config.name + '.apk');
+
+  var log = '';
+  return exec(apksignerCmd.join(' '), {
     cwd: process.cwd(),
     env: process.env,
     maxBuffer: 1024 * 500
   })
     .then(function (res) {
-      if (res.stderr.length > 0) throw stderr
-      log += res.stdout
+      if (res.stderr.length > 0) throw stderr;
+      log += res.stdout;
     })
     .catch(function (err) {
-      console.log('jarsigner failed:', err)
+      console.log('jarsigner failed:', err);
       throw err; // don't swallow; cancel here
     })
     .then(function () {
@@ -36,23 +39,22 @@ module.exports = function (config) {
         cwd: process.cwd(),
         env: process.env,
         maxBuffer: 1024 * 500
-      })
+      });
     })
     .then(function (res) {
-      if (res.stderr.length > 0) throw stderr
-      log += res.stdout
+      if (res.stderr.length > 0) throw stderr;
+      log += res.stdout;
     })
     .catch(function (err) {
-      console.log('zipalign failed:', err)
+      console.log('zipalign failed:', err);
       throw err; // don't swallow; cancel here
     })
     .then(function () {
       if (config.log) {
-        return fs.write(config.log, log)
-          .then(function () {
-            return fs.read(config.log)
-          })
+        return fs.write(config.log, log).then(function () {
+          return fs.read(config.log);
+        });
       }
-      return true
-    })
-}
+      return true;
+    });
+};
